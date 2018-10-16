45d566295886243a64298bf75666cfcd12ab7929
Therese Federl <theresehfederl@gmail.com>
2018-10-16 16:39:28 +0200

    moved logic for welcome message from screen to component #29

--- components/WelcomeMessage.js
+++ components/WelcomeMessage.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import {Text, View, Dimensions, StyleSheet} from 'react-native';
+import React from "react";
+import { Text, View, Dimensions, StyleSheet } from "react-native";
 
-const screenWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get("window").width;
 
 class WelcomeMessage extends React.Component {
-    constructor(props) {
-        super(props)
+  state = {
+    currentHour: 0
+  };
+
+  componentDidMount() {
+    this.setState({
+      currentHour: new Date().getHours()
+    });
+  }
+
+  // checks time of day and set customisedMessage state
+  checkTimeOfDay = () => {
+    let currentHour = this.state.currentHour;
+    console.log(currentHour);
+    if (currentHour < 5) {
+      return "Maybe time to get some sleep? 😴 ";
+    } else if (currentHour < 12) {
+      return "Good morning! \nLet's get started checking off tasks ✅";
+    } else if (currentHour < 19) {
+      return "Good afternoon! \nKeep up the good work 💪 ";
+    } else {
+      return "Good evening! \nTime to relax 😊";
     }
-    
-    render() { 
-        return ( 
-            <View>
-                <Text style={styles.text}>{this.props.customisedMessage}</Text>
-            </View>
-         );
-    }
+  };
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>{this.checkTimeOfDay()}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    
   text: {
     fontSize: 24,
     paddingBottom: 50,
     lineHeight: 30,
-    width: 0.7 * screenWidth,
-  }})
- 
+    width: 0.7 * screenWidth
+  }
+});
+
 export default WelcomeMessage;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,92 +1,55 @@
-import React from 'react';
-import {View, StyleSheet } from 'react-native';
-import NavigationButton from '../components/NavigationButton';
-import WelcomeMessage from '../components/WelcomeMessage';
-
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import NavigationButton from "../components/NavigationButton";
+import WelcomeMessage from "../components/WelcomeMessage";
 
 class HomeScreen extends React.Component {
-  
-  state = { 
-    customisedMessage: ""
- }  
-  static navigationOptions= {
-      title: 'Home',
-    };
-    
-   componentDidMount() {
-       this.checkTimeOfDay();
-   }
-  
-  // check time of day and set customisedMessage state
-  checkTimeOfDay = () => {
-      let currentHour = new Date().getHours();
-      if (currentHour < 5) {
-        this.setState({
-          customisedMessage: "Maybe time to get some sleep? 😴 "
-      })
-      }
-      else if (currentHour < 12) {
-          this.setState({
-              customisedMessage: "Good morning! \nLet's get started checking off tasks ✅"
-          })
-      } else if (currentHour < 19) {
-          this.setState({
-              customisedMessage: "Good afternoon! \nKeep up the good work 💪 "
-          })
-      } else {
-          this.setState({
-              customisedMessage: "Good evening! \nTime to relax 😊"
-          })
-      }
+  static navigationOptions = {
+    title: "Home"
+  };
 
-  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.messageContainer}>
+          <WelcomeMessage style={styles.message} />
 
-    render() {
-      return (
-        <View style={styles.container}>
-          <View style={styles.messageContainer}>
-            <WelcomeMessage style={styles.message} customisedMessage={this.state.customisedMessage}/>
-          
           <View style={styles.buttonContainer}>
-            <NavigationButton 
+            <NavigationButton
               onPress={() => {
-                this.props.navigation.navigate('Tasks');
+                this.props.navigation.navigate("Tasks");
               }}
-              title = "TODOs"
+              title="TODOs"
             />
-            <NavigationButton 
+            <NavigationButton
               onPress={() => {
-                this.props.navigation.navigate('Pedometer');
+                this.props.navigation.navigate("Pedometer");
               }}
-              title = "Pedometer"
+              title="Pedometer"
             />
           </View>
-         
         </View>
-
-
-        </View>
-        
-      );
-    }
+      </View>
+    );
   }
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "space-between",
-      backgroundColor: "#fff",
-    },
-    messageContainer: {
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: '#fff',
-      marginTop: 40,
-    },
-    buttonContainer: {
-      marginTop: 30,
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#fff"
+  },
+  messageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    marginTop: 40
+  },
+  buttonContainer: {
+    marginTop: 30
+  }
+});
 
-  export default HomeScreen;
+export default HomeScreen;
